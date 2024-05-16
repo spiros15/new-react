2d7f5083eeeb1f5407de1d2a4cf88db2f56df325
Spiros <spirosppas99@gmail.com>
2024-05-17 01:07:47 +0300

    changes3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+import Login from "./Components/Login/Login";
+import SignUp from "./Components/Sign_Up/Sign_Up";
+import Landing_Page from "./Components/Landing_Page/LandingPage.js";
+import Navbar from './Components/Navbar/Navbar';
+import ReviewForm from "./Components/ReviewForm/ReviewForm"
+import Home from './Components/Home/Home';
+import Layout from "./Components/Landing_Page/LandingPage";
+
+
+function App() {
+
+    return (
+      <div className="App">
+          <BrowserRouter>
+            <Navbar/>
+                <Routes>
+                <Route path="/" element={<Layout/>}/>
+                <Route path="/Home" element={<Home/>} />
+                <Route path="/SignUp" element={<SignUp/>}/>
+                <Route path="/Login" element={<Login/>}/>
+                <Route path="/ReviewForm" element={<ReviewForm/>}/>
+                <Route path="/SignUp" element={<SignUp/>}/>
+                </Routes>
+          
+          </BrowserRouter>
+         
+      </div>
+    );
+  }
+  
+  export default App;
+
+
+
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> </>
   );
 }
 
 export default App;
+*/
+/*
+import React, { useEffect } from 'react';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Landing_Page from './Components/Landing_Page/Landing_Page';
+
+import Login from './Components/Login/Login';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
+//import InstantConsultationBooking from './Components/InstantConsultation/InstantConsultationBooking';
+import InstantConsultation from './Components/InstantConsultation/InstantConsultation';  
+import BookingConsultation from './Components/BookingConsultation';  
+import Navbar from './Components/Navbar/Navbar';
+
+function App() {
+
+  return (
+    <div className="App">
+        <BrowserRouter>
+        
+          <Navbar/>
+              <Routes>
+              <Route path="/" element={<Landing_Page/>}/>
+              <Route path="/Login" element={<Login/>}/>
+              <Route path="/Sign_Up" element={<Sign_Up/>}/>
+              <Route path="/InstantConsultation" element={<InstantConsultation/>} />
+              <Route path="/BookingConsultation" element={<BookingConsultation/>} />
+              </Routes>
+            
+        </BrowserRouter>
+       
+    </div>
+  );
+}
+
+export default App;*/
